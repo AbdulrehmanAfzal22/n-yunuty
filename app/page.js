ac41fa026e8c7cf02bbe8837d862fd2688c2f9c6
Abdulrehman Afzal <you@example.com>
2026-02-02 01:56:46 +0500

    commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Navbar from "../app/page/navbar/page.jsx";
+import Hero from "../app/page/hero/page.jsx";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
